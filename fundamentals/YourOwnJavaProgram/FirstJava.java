643f37f4c6fca3b4ad3ab1d9292b6b48833f5a4e
winterilee <plumtree9612@gmail.com>
2023-06-07 16:23:01 -0700

    completed the assignment: First Java Program

---
+++ fundamentals/YourOwnJavaProgram/FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava {
+    public static void main(String[] args){
+        System.out.println("My name is Winter Lee.");
+        System.out.println("I am in my 30s.");
+        System.out.println("I moved around a lot, so I don't have a hometown. But if I have to choose, it would probably be Orange County, CA.");
+    }
+}
