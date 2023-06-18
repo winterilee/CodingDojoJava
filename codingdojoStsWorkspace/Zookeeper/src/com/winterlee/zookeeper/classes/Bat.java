729fc4d639660c1783303f574bd5af310740c764
winterilee <plumtree9612@gmail.com>
2023-06-18 05:56:12 -0700

    completed the assignment: Zookeeper

--- codingdojoStsWorkspace/Zookeeper/src/com/winterlee/zookeeper/classes/Bat.java
+++ codingdojoStsWorkspace/Zookeeper/src/com/winterlee/zookeeper/classes/Bat.java
@@ -2,4 +2,23 @@ package com.winterlee.zookeeper.classes;
 
 public class Bat extends Mammal {
 	
+	public Bat() {
+		this.energyLevel = 300;
+	}
+	
+	public void fly() {
+		this.energyLevel -= 50;
+		System.out.println("*flutter* The bat took off and is flying. Its energy level has been decreased by 50.");
+	}
+	
+	public void eatHumans() {
+		this.energyLevel += 25;
+		System.out.println("*crack* The bat is eating humans. Its energy level has been increased by 25");
+	}
+	
+	public void attackTown() {
+		this.energyLevel -= 100;
+		System.out.println("*thrash* The bat is attacking a town. Its energy level has been decreased by 100");
+	}
+	
 }
--- codingdojoStsWorkspace/Zookeeper/src/com/winterlee/zookeeper/controllers/BatTest.java
+++ codingdojoStsWorkspace/Zookeeper/src/com/winterlee/zookeeper/controllers/BatTest.java
@@ -1,9 +1,22 @@
 package com.winterlee.zookeeper.controllers;
 
+import com.winterlee.zookeeper.classes.Bat;
+
 public class BatTest {
 
 	public static void main(String[] args) {
-		
+		Bat bat1 = new Bat();
+		bat1.displayEnergy();
+		for (int i = 0; i < 3; i++) {
+			bat1.attackTown();
+		}
+		for (int i = 0; i < 2; i++) {
+			bat1.eatHumans();
+		}
+		for (int i = 0; i < 2; i++) {
+			bat1.fly();
+		}
+		bat1.displayEnergy();
 
 	}
 
