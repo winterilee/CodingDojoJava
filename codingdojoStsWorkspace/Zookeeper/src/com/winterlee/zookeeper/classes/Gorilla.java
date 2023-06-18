159157911df189562393896aa0da648b362136dd
winterilee <plumtree9612@gmail.com>
2023-06-18 05:43:38 -0700

    making a progress on the class, Gorilla and its Test

--- codingdojoStsWorkspace/Zookeeper/src/com/winterlee/zookeeper/classes/Gorilla.java
+++ codingdojoStsWorkspace/Zookeeper/src/com/winterlee/zookeeper/classes/Gorilla.java
@@ -2,4 +2,19 @@ package com.winterlee.zookeeper.classes;
 
 public class Gorilla extends Mammal {
 	
+	public void throwSomething() {
+		this.energyLevel -= 5;
+		System.out.println("The gorilla is throwing something. Its energy level has been decreased by 5.");
+	}
+	
+	public void eatBananas() {
+		this.energyLevel += 10;
+		System.out.println("The gorilla is eating bananas. Its energy level has been increased by 10.");
+	}
+	
+	public void climb() {
+		this.energyLevel -= 10;
+		System.out.println("The gorilla is climbing. Its energy level has been decreased by 10.");
+	}
+	
 }
--- codingdojoStsWorkspace/Zookeeper/src/com/winterlee/zookeeper/controllers/GorillaTest.java
+++ codingdojoStsWorkspace/Zookeeper/src/com/winterlee/zookeeper/controllers/GorillaTest.java
@@ -1,9 +1,20 @@
 package com.winterlee.zookeeper.controllers;
 
+import com.winterlee.zookeeper.classes.Gorilla;
+
 public class GorillaTest {
 
 	public static void main(String[] args) {
-		
+		Gorilla gorilla1 = new Gorilla();
+		gorilla1.displayEnergy();
+		for (int i = 0; i < 3; i++) {
+			gorilla1.throwSomething();
+		}
+		for (int i = 0; i < 2; i++) {
+			gorilla1.eatBananas();
+		}
+		gorilla1.climb();
+		gorilla1.displayEnergy();
 
 	}
 
