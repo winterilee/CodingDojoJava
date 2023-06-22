b3ee0945811f0f81b5544e1b2924d61cb7aaf9ab
winterilee <plumtree9612@gmail.com>
2023-06-21 20:00:55 -0700

    completed the assignment: Daikichi Path Variables

---
+++ codingdojoStsWorkspace/DaikichiPathVariables/src/main/java/com/winterlee/daikichipathvariables/DaikichiController.java
@@ -0,0 +1,51 @@
+package com.winterlee.daikichipathvariables;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/daikichi")
+public class DaikichiController {
+
+	@RequestMapping("")
+	public String index() {
+		return "Welcome!";
+	}
+	
+	@RequestMapping("/today")
+	public String today() {
+		return "Today you will find luck in all your endeavors!";
+	}
+	
+	@RequestMapping("/tomorrow")
+	public String tomorrow() {
+		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	@RequestMapping("/travel")
+	public String travelMissing() {
+		return "Invalid input.";
+	}
+	
+	@RequestMapping("/travel/{city}")
+	public String travel(@PathVariable("city") String city) {
+		return String.format("Congratulations! You will soon travel to %s!", city);
+	}
+	
+	@RequestMapping("/lotto")
+	public String lottoMissing() {
+		return "Invalid input.";
+	}
+	
+	@RequestMapping("/lotto/{num}")
+	public String lotto(@PathVariable("num") Integer num) {
+		if (num % 2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers.";
+		}
+		else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		}
+	}
+	
+}
