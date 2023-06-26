d7236095a099acef92c12e154e23d44a84d8b998
winterilee <plumtree9612@gmail.com>
2023-06-25 20:50:32 -0700

    attempted to put all functionalities into two methods

--- codingdojoStsWorkspace/NinjaGoldGame/src/main/java/com/winterlee/ninjagoldgame/controllers/NinjaGoldGameController.java
+++ codingdojoStsWorkspace/NinjaGoldGame/src/main/java/com/winterlee/ninjagoldgame/controllers/NinjaGoldGameController.java
@@ -1,11 +1,15 @@
 package com.winterlee.ninjagoldgame.controllers;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Random;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -16,53 +20,94 @@ public class NinjaGoldGameController {
 	@GetMapping("")
 	public String index(HttpSession session) {
 		if(session.getAttribute("gold") == null) {
-			session.setAttribute("gold", 0);			
+			session.setAttribute("gold", 0);
 		}
 		
 		return "index.jsp";
 	}
 	
-	@PostMapping("/farm")
-	public String farm(HttpSession session) {
-		Random r = new Random();
-		int min = 10;
-		int max = 20 + 1;
-		int rNum = r.nextInt(max-min) + min;
-		System.out.println(rNum);
-		session.setAttribute("gold", (Integer) session.getAttribute("gold")+ rNum);
+	@PostMapping("/action")
+	public String action(HttpSession session,
+			@RequestParam(value="farm", required=false) String farm,
+			@RequestParam(value="cave", required=false) String cave,
+			@RequestParam(value="house", required=false) String house,
+			@RequestParam(value="quest", required=false) String quest) {
 		
-		return "redirect:/";
-	}
-	@PostMapping("/cave")
-	public String cave(HttpSession session) {
-		Random r = new Random();
-		int min = 5;
-		int max = 10 + 1;
-		int rNum = r.nextInt(max-min) + min;
-		System.out.println(rNum);
-		session.setAttribute("gold", (Integer) session.getAttribute("gold")+ rNum);		
+		ArrayList<String> activities = new ArrayList<String>();
 		
-		return "redirect:/";
-	}
-	@PostMapping("/house")
-	public String house(HttpSession session) {
-		Random r = new Random();
-		int min = 2;
-		int max = 5 + 1;
-		int rNum = r.nextInt(max-min) + min;
-		System.out.println(rNum);
-		session.setAttribute("gold", (Integer) session.getAttribute("gold")+ rNum);
+		if(farm != null && cave == null && house == null && quest == null) {
+			Random r = new Random();
+			Date d = new Date();
+			SimpleDateFormat formatter = new SimpleDateFormat("MMMM d Y h:mm:ss a");
+			int min = 10;
+			int max = 20;
+			int rNum = r.nextInt(max-(min+1)) + min;
+			String cDate = formatter.format(d);
+			session.setAttribute("gold", (Integer) session.getAttribute("gold")+ rNum);
+			String activity = String.format("You entered a farm and earned %s gold. (%s)\n", rNum, cDate);
+			if(session.getAttribute("activities") != null) {
+				activities = (ArrayList<String>) session.getAttribute("activities");
+			}
+			activities.add(activity);
+			session.setAttribute("activities", activities);
+		}
 		
-		return "redirect:/";
-	}
-	@PostMapping("/quest")
-	public String quest(HttpSession session) {
-		Random r = new Random();
-		int min = -50;
-		int max = 50 + 1;
-		int rNum = r.nextInt(max-min) + min;
-		System.out.println(rNum);
-		session.setAttribute("gold", (Integer) session.getAttribute("gold")+ rNum);
+		if(farm == null && cave != null && house == null && quest == null) {
+			Random r = new Random();
+			Date d = new Date();
+			SimpleDateFormat formatter = new SimpleDateFormat("MMMM d Y h:mm:ss a");
+			int min = 5;
+			int max = 10;
+			int rNum = r.nextInt(max-(min+1)) + min;
+			String cDate = formatter.format(d);
+			session.setAttribute("gold", (Integer) session.getAttribute("gold")+ rNum);
+			String activity = String.format("You entered a cave and earned %s gold. (%s)\n", rNum, cDate);
+			if(session.getAttribute("activities") != null) {
+				activities = (ArrayList<String>) session.getAttribute("activities");
+			}
+			activities.add(activity);
+			session.setAttribute("activities", activities);
+		}
+		
+		if(farm == null && cave == null && house != null && quest == null) {
+			Random r = new Random();
+			Date d = new Date();
+			SimpleDateFormat formatter = new SimpleDateFormat("MMMM d Y h:mm:ss a");
+			int min = 2;
+			int max = 5;
+			int rNum = r.nextInt(max-(min+1)) + min;
+			String cDate = formatter.format(d);
+			session.setAttribute("gold", (Integer) session.getAttribute("gold")+ rNum);
+			String activity = String.format("You entered a house and earned %s gold. (%s)\n", rNum, cDate);
+			if(session.getAttribute("activities") != null) {
+				activities = (ArrayList<String>) session.getAttribute("activities");
+			}
+			activities.add(activity);
+			session.setAttribute("activities", activities);
+		}
+		
+		if(farm == null && cave == null && house == null && quest != null) {
+			Random r = new Random();
+			Date d = new Date();
+			SimpleDateFormat formatter = new SimpleDateFormat("MMMM d Y h:mm:ss a");
+			int min = -50;
+			int max = 50;
+			int rNum = r.nextInt(max-(min+1)) + min;
+			String cDate = formatter.format(d);
+			session.setAttribute("gold", (Integer) session.getAttribute("gold")+ rNum);
+			if(session.getAttribute("activities") != null) {
+				activities = (ArrayList<String>) session.getAttribute("activities");
+			}
+			if(rNum < 0) {
+				String activity = String.format("You failed a quest and lost %s gold. (%s)\n", (rNum*-1), cDate);
+				activities.add(activity);
+			}
+			else {
+				String activity = String.format("You completed a quest and earned %s gold. (%s)\n", rNum, cDate);
+				activities.add(activity);
+			}
+			session.setAttribute("activities", activities);
+		}
 		
 		return "redirect:/";
 	}
--- codingdojoStsWorkspace/NinjaGoldGame/src/main/webapp/WEB-INF/index.jsp
+++ codingdojoStsWorkspace/NinjaGoldGame/src/main/webapp/WEB-INF/index.jsp
@@ -19,35 +19,37 @@
 		<div>
 			<h5>Farm</h5>
 			<p>(earns 10-20 gold)</p>
-			<form action="/farm" method="post">
-				<button>Find Gold!</button>
+			<form action="/action" method="post">
+				<input type="submit" value="Find Gold!" name="farm"/>
 			</form>
 		</div>
 		<div>
 			<h5>Cave</h5>
 			<p>(earns 5-10 gold)</p>
-			<form action="/cave" method="post">
-				<button>Find Gold!</button>
+			<form action="/action" method="post">
+				<input type="submit" value="Find Gold!" name="cave"/>
 			</form>
 		</div>
 		<div>
 			<h5>House</h5>
 			<p>(earns 2-5 gold)</p>
-			<form action="/house" method="post">
-				<button>Find Gold!</button>
+			<form action="/action" method="post">
+				<input type="submit" value="Find Gold!" name="house"/>
 			</form>
 		</div>
 		<div>
 			<h5>Quest</h5>
 			<p>(earns/takes 0-50 gold)</p>
-			<form action="/quest" method="post">
-				<button>Find Gold!</button>
+			<form action="/action" method="post">
+				<input type="submit" value="Find Gold!" name="quest"/>
 			</form>
 		</div>
 	</div>
 	<div>
 		<h5>Activities:</h5>
-		<p>"placeholder for activities"</p>
+		<c:forEach var="activity" items="${activities}">
+			<p><c:out value="${activity}"></c:out></p>
+		</c:forEach>
 	</div>
 	<form action="/reset">
 		<button>Restart</button>
